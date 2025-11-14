f53659bef65e8a198ca1e7377e8f7a0d57fc80d6
Kelvis Fauza <kelvisfauza@gmail.com>
2025-11-14 13:46:31 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
-  optimizeDeps: {
-    include: ['firebase/compat/app', 'firebase/compat/firestore']
+  resolve: {
+    alias: {
+      'firebase/compat/app': path.resolve(__dirname, 'node_modules/firebase/compat/app/dist/esm/index.esm.js'),
+      'firebase/compat/firestore': path.resolve(__dirname, 'node_modules/firebase/compat/firestore/dist/esm/index.esm.js')
+    }
   }
 })
